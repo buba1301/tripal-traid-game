463e8ffd19c37af9ea0be0948ca90850ceeddcc4
Boris Muzalevskii <bmuza57@gmail.com>
2022-04-17 13:01:56 +0300

    Fix Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,24 +9,22 @@ const MENU = ['Menu 1', 'Menu 2', 'Menu 3', 'Menu 4'];
 
 const Header = () => {
   return (
-    <Container>
-      <header className={s.root}>
-        <div className={s.headerWrap}>
-          <div className={s.header}>
-            <div className={s.logo}>
-              <img src={logo} alt='Logo' />
-            </div>
-            <ul className={s.nav}>
-              {MENU.map((element) => (
-                <li key={element}>
-                  <a href='#'>{element}</a>
-                </li>
-              ))}
-            </ul>
+    <header className={s.root}>
+      <div className={s.header}>
+        <Container className={s.headerWrap}>
+          <div className={s.logo}>
+            <img src={logo} alt='Logo' />
           </div>
-        </div>
-      </header>
-    </Container>
+          <ul className={s.nav}>
+            {MENU.map((element) => (
+              <li key={element}>
+                <a href='#'>{element}</a>
+              </li>
+            ))}
+          </ul>
+        </Container>
+      </div>
+    </header>
   );
 };
 
